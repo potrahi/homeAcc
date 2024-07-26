ec66da3b410d45d83f7f63cae46f342885cb3801
Danyil Nikulin <dnikulin@vendavo.com>
2024-07-26 13:24:55 +0200

    made gear icon for settings button rotate when clicking

--- frontend/src/components/Settings/SettingsButton.tsx
+++ frontend/src/components/Settings/SettingsButton.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OpenModalButton from '../OpenModalButton';
 import './styles/SettingsButton.css';
 import gear from '../../assets/gear-svgrepo-com.svg';
 
 const SettingsButton: React.FC = () => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleClick = () => {
+        setIsClicked(true);
+        setTimeout(() => setIsClicked(false), 250);
+    }
 
     return (
         <OpenModalButton contentId="SettingsForm">
-            <button className="settings-button">
+            <button
+                className={`settings-button ${isClicked ? 'clicked' : ''}`}
+                onClick={handleClick}
+            >
                 <img src={gear} alt="Settings" className="gear-icon" />
             </button>
         </OpenModalButton>
