4a9daf2191e214eebc0b80e1a99795354ab44e6d
potrahi <nikulinde79@gmail.com>
2024-07-28 21:40:59 +0200

    added package for debugging

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 import { Pool } from "pg";
 import userRoutes from "./routes/user";
 import balanceRoutes from "./routes/balance";
+import spendingRoutes from "./routes/spending";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -16,14 +20,20 @@ const pool: Pool = new Pool({
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.pool = pool;
-  next();
-});
-
 app.use("/users", userRoutes(pool));
 app.use("/balances", balanceRoutes(pool));
+app.use("/spending", spendingRoutes(pool));
 
-app.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error("Error acquiring client", err.stack);
+    process.exit(-1);
+  } else {
+    console.log("Connected to the database");
+    release();
+
+    app.listen(port, () => {
+      console.log(`Server started at http://localhost:${port}`);
+    });
+  }
 });
