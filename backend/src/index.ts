627990d54a5ae197131627e0bacc5b637ce15515
potrahi <nikulinde79@gmail.com>
2024-07-28 20:53:03 +0200

    added balanceService.ts, balanceController.ts and routes/balance.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Pool } from "pg";
 import userRoutes from "./routes/user";
+import balanceRoutes from "./routes/balance";
 
 const app = express();
 const port = 3000;
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/users", userRoutes(pool));
+app.use("/balances", balanceRoutes(pool));
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
