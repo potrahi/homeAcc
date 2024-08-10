9799d989f8aef01c9fad57225086492710ea3eab
potrahi <nikulinde79@gmail.com>
2024-08-10 19:51:33 +0200

    updated spendings table width

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,14 +5,15 @@ import react from "@vitejs/plugin-react";
 export default ({ mode }: ConfigEnv) => {
   const env = loadEnv(mode, process.cwd());
 
-  console.log("VITE_API_URL:", env.VITE_API_URL);
-
   return defineConfig({
     plugins: [react()],
     define: {
       "process.env": {
         VITE_API_URL: env.VITE_API_URL,
       },
+    },
+    server: {
+      host: true,
     },
   });
 };
